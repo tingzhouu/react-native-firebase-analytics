688a6f26b6ac8f5f851c72b9cc636b1c74939d38
tingzhouu <limtingzhou@gmail.com>
2020-03-22 11:37:37 +0800

    add ios configuration for 'react-native-onesignal'

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Lim Ting Zhou on 21/3/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
